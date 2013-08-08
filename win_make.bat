855477e4b8328dbdea5ef0b0c95e131eac6d87c3
Gitcessing <gitcessing@gmail.com>
2013-08-08 17:01:18 +0100

    recommitting so that EOL changes to CRLF

--- win_make.bat
+++ win_make.bat
@@ -3,7 +3,7 @@ javac -d bin -target 1.6 -source 1.6 -sourcepath src -cp lib/core.jar src/simpli
 echo %ERRORLEVEL%
 if ERRORLEVEL 1 GOTO L_FAILURE
 
-echo ############ creating jar ###############
+echo ############ creating jar ###############.
 cd bin
 jar cfv ../dist/simplicity/library/simplicity.jar  * 
 cd ..
