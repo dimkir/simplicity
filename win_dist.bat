52c91f1a7161e49056987c0bdc981173b9355c97
Gitcessing <gitcessing@gmail.com>
2013-08-08 16:57:36 +0100

    fixes unnecessary space bug on win_dist.bat

--- win_dist.bat
+++ win_dist.bat
@@ -2,10 +2,10 @@
 REM *************************************************************************
 REM *************************************************************************
 REM ******** HERE YOU MUST SPECIFY path to your [libraries] folder  *********
-REM ********  inside your processing sketchbook directory     ***************
+REM ********  inside your processing sketchbook directory      **************
 REM *************************************************************************
 REM *************************************************************************
-SET SKETCHBOOK_LIBRARIES_FOLDER ="C:\Users\Local User\Documents\Processing\libraries"
+SET SKETCHBOOK_LIBRARIES_FOLDER="C:\Users\Local User\Documents\Processing\libraries"
 REM **** Important: always put " (doublequotes) around your full pathname. ****
 
 
