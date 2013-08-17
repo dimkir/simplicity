db681b845310017227713f6ce0988692f4f19474
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-08-17 21:30:54 +0100

    adds variable compiler path for linux/windows, java source file list is generated by java and passed to compiler, cosmetic messages, switches off verbose mode, retrieving path to preferences.txt in linux as well

--- LibMake.java
+++ LibMake.java
@@ -4,6 +4,8 @@ import java.io.StringReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.BufferedReader;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.io.InputStreamReader;
 import java.io.InputStream;
 import java.io.IOException;
@@ -107,20 +109,21 @@ class LibMake
 		void compileClasses(int lineOffset) throws LibMakeEx
 		{
 				String wkd = System.getProperty("user.dir");
-				System.out.println("Working Directory: " + wkd);
+				dprintln("Working Directory: " + wkd, lineOffset);
+                                dprintln("Compiling...", lineOffset);
+                                String srcDir = wkd + File.separator  + "src"; 
 				    Process p;
-					String cmd = "../java/bin/javac -verbose -d bin -target 1.6 -source 1.6 -sourcepath src -cp ../core/library/core.jar src/simplicity/*.java  -bootclasspath ../java/lib/rt.jar";
-					String[] arcmd = { "../java/bin/javac",
-								
-							   "-verbose",
+					//String cmd = "../java/bin/javac -verbose -d bin -target 1.6 -source 1.6 -sourcepath src -cp ../core/library/core.jar src/simplicity/*.java  -bootclasspath ../java/lib/rt.jar";
+					String[] arcmd = {
+                                                            getJavacPath(),
+							   // "-verbose",  // with -verbose on when running on Windows it never returns
 							   "-d", "bin",
 							   "-target" , "1.6",
 							   "-source", "1.6",
-							   "-sourcepath", wkd + File.separator  + "src",
+							   "-sourcepath", srcDir ,  // TODO: should this be in dquotes? What if full path has spaces?
 							   "-cp", "../core/library/core.jar",
 							   "-bootclasspath",  "../java/lib/rt.jar",
-							   "src/simplicity/BouncingBall.java"
-
+                                                           getFileListAsString(srcDir)
 							};
 					try
 					{
@@ -128,14 +131,15 @@ class LibMake
 					 p = Runtime.getRuntime().exec(arcmd);
 					  int rez = p.waitFor();
 
-					  dprintln("Exit result returned: " + rez, lineOffset);
+					  
 					  
 					  if ( rez != 0 ){  // error
 					     // output error buffer
 						 printStream(p.getErrorStream(), lineOffset);
 						 throw new LibMakeEx("Compile failed with code: " + rez);
 					  }
-					  
+					  dprintln("Compilation of library was successful.",  lineOffset);
+                                          dprintln("Compiler exit code (" + rez + ")", lineOffset);
 					  printStream(p.getInputStream(), lineOffset);
 					  // else success
 					 
@@ -176,19 +180,20 @@ class LibMake
 					File jarExePath = new File("../java/bin/jar");
 					String jarExe = null;
 					try{
-					 jarExe = jarExePath.getCanonicalPath();
+					 jarExe = jarExePath.getCanonicalPath(); // how the hell this canonical "works" if the 
+                                                                                  // file doesn't really exist
 					}
 					catch(IOException ioex){
 					   // this is kinda fatal if we can't get canonical path.
 					   throw new RuntimeException(ioex);
 					}
-					dprintln("Jar exe is: [" + jarExe + "]", outputPaddingOffset);
+					dprintln("JAR executable is: [" + jarExe + "]", outputPaddingOffset);
 					
 					
 					//String jarPath = "c:\\Users\\Ernesto Guevara\\Desktop\\ProcessingBin\\processing-2.0.1\\java\\bin\\jar.exe";
 					//String cmd = jarPath + "  " + " cfv ../dist/simplicity/library/simplicity.jar  * ";
 					//String cmd = "jar cfv ../dist/simplicity/library/simplicity.jar  * ";
-					String cmd = jarExe +  " cfv ../dist/simplicity/library/simplicity.jar  * ";
+					//String cmd = jarExe +  " cfv ../dist/simplicity/library/simplicity.jar  * ";
 					String[] cmdArray = { jarExe, "cfv", "../dist/simplicity/library/simplicity.jar", "*" };
 					try
 					{
@@ -287,15 +292,41 @@ class LibMake
 			fetches environment variable %APPROAMING%? or whatever it's called.
 		*/
 		private static String getPreferencesTxtPath(){
+                    int os = OS.getOS();
+                    switch ( os ){
+                        case OS.LINUX:
+                                return getPreferencesTxtPathFromLinux();
+                        case OS.WINDOWS:
+                                return getPreferencesTxtPathFromWindows();
+                        case OS.MACOS:
+                                throw new RuntimeException("getPreferencesTxtPath(): this only works on Linux/Windows currently.");
+                    }
+                    throw new RuntimeException("We shouldn't have got to this line");
+		}
+
+                /**
+                 * Fetches filepath for preferences.txt on linux.
+                 * (From user dir from subdirectory .processing
+                 */
+                private static String getPreferencesTxtPathFromLinux(){
+		    String USER_DIR = System.getProperty("user.dir");
+			if ( USER_DIR == null ){
+			   throw new RuntimeException("Error cannot get 'user.dir' property. Are you running on Linux?");
+			}
+			return USER_DIR + File.separator + ".processing" +  File.separator + "preferences.txt";                    
+                }                
+                
+                /**
+                 * Fetches preferences txt file on Windows. 
+                 * (From APPDATA environment variable)
+                 */
+                private static String getPreferencesTxtPathFromWindows(){
 		    String APPDATA_DIR = System.getenv("APPDATA");
 			if ( APPDATA_DIR == null ){
-			   throw new RuntimeException("Error cannot get APPDATA environment variable. This script so far works on Windows." + 
-							" for support on linux machine, modify getPreferencesTxtPath() so that it returns path to " + 
-							" preferences.txt on Linux");
+			   throw new RuntimeException("Error cannot get APPDATA environment variable are you running on Windows?");
 			}
-			return APPDATA_DIR + File.separator + "Processing" +  File.separator + "preferences.txt";
-		}
-		
+			return APPDATA_DIR + File.separator + "Processing" +  File.separator + "preferences.txt";                    
+                }
 		
 		//*******************************************************************************************
 		//*******************************************************************************************
@@ -401,12 +432,90 @@ class LibMake
 			sb.append(msg);
 			return sb.toString();
 		}
-		
-		static File getSketchBookFolder(){
-		    return null;
-		}
-		
-		
+
+                
+
+                /**
+                 * Just returns path to javac (or javac.exe) relative to current path.
+                 * OS dependent.
+                 */
+                private static String getJavacPath() {
+                    if ( OS.getOS() == OS.LINUX ){
+                        return "../java/bin/javac";
+                    }
+                    else if ( OS.getOS() == OS.WINDOWS ){
+                        return "../java/bin/javac.exe";
+                    }
+                    throw new RuntimeException("only work with Linux and Windows");
+                }
+
+                
+                /**
+                 *  Gets listing of all files in the directory and it's subfolders,
+                 * src/somefile.java
+                 * src/simplicity/BouncingBall.java
+                 * src/simplicity/BouncingBall1.java
+                 * src/simplicity/BouncingBall2.java
+                 * and returns the list as string 
+                 * "src/somefile.java src/simplicity/BouncingBall.java AND_SO_ON"
+                 * 
+                 * We asssume that neither "src" dir neither subdirectories nor files
+                 * have spaces in their names
+                 */
+                static String getFileListAsString(String srcDir) {
+                    ArrayList<String> filePathes = new ArrayList<String>();
+                    File root = new File(srcDir);
+                    traverse(root, filePathes);
+                    return arlisToString(filePathes, ' ');
+                }
+                
+                public static void traverse(File dir, ArrayList<String> fileCollection){
+                    File[] files;
+                    File[] dirs;
+                    // get directory contents
+                    files = dir.listFiles(C_FILE_FILTER);
+                    dirs = dir.listFiles(C_DIR_FILTER);
+                    
+                    // files: add to list
+                    for(File f: files){
+                        fileCollection.add(f.getPath());
+                    }
+                    // directories: traverse
+                    for(File d: dirs ){
+                        traverse(d, fileCollection);
+                    }
+                }
+
+                /**
+                 * Concatenates arlis<String> into string.
+                 * Also puts stirng in quotes if it has space in it.
+                 * this is to specify pathes which have spaces in them. Dunno if it will work on linux...
+                 * @return concatenated (with space) String.
+                 */
+                static String arlisToString(ArrayList<String> arlis, char separChar){
+                     StringBuilder sb = new StringBuilder();
+                     for(int i = 0 ; i < arlis.size() ; i++){
+                         String s = arlis.get(i);
+                         if ( i != 0 ){
+                              sb.append(separChar);
+                         }
+                         
+                         if ( s.contains(" ") ){
+                               // adds double quotes on both sides of the line
+                              
+                                sb.append('"');
+                                sb.append(s);
+                                sb.append('"');
+                         }
+                         else{
+                                sb.append(s);    
+                         }
+                                 
+                         
+                     }
+                     return sb.toString();
+                }
+	
 		/**
 		* Our local exception
 		*/
@@ -477,6 +586,85 @@ class LibMake
 					destination.close();
 				}
 			}
-		}		
+		}	
+                
+
+                /**
+                 * Just helper class providing static methods for detecting
+                 * current OS.
+                 */
+                static class OS
+                {
+                     public static final int UNINITIALIZED = 0;
+                     public static final int LINUX = 1;
+                     public static final int WINDOWS = 2;
+                     public static final int MACOS = 3;
+                     
+                     private static int smCurrentOs = UNINITIALIZED;
+                     
+                     public static int getOS(){
+                          if ( smCurrentOs == UNINITIALIZED ){
+                              detectOS();
+                          }
+                          return smCurrentOs;
+                     }
+                     
+                     private static void detectOS(){
+                          String osName = System.getProperty("os.name");
+                          if ( osName.toLowerCase().contains("windows") ){
+                              smCurrentOs = WINDOWS;
+                              return;
+                          }
+                          
+                          if ( osName.toLowerCase().contains("mac")){
+                              smCurrentOs = MACOS;
+                              return;
+                          }
+                          
+                          if ( osName.toLowerCase().contains("linux") ){
+                              smCurrentOs = LINUX;
+                              return;
+                          }
+                          
+                          System.out.println("Your os name is: [" + osName + "], doesn't seem to be windows or macos, we assume it's some type of Linux");
+                          smCurrentOs = LINUX;
+                          
+                     }
+                     
+                }// class OS
+                
+
+                /**
+                 * Checks and accepts *.java or *.JAVA files.
+                 */
+                private final static FileFilter C_FILE_FILTER = new FileFilter() {
+                    @Override
+                    public boolean accept(File pathname) {
+                               if ( pathname.isDirectory()){
+                                   return false;
+                               }
+                               
+                               return pathname.getName().toLowerCase().endsWith(".java");
+                               //                        ^^^^^^^^^^^
+                               //                keep in mind that we are using case insensitive
+                               //                   verification here.
+                    }
+                };
+                
+                /**
+                 * Returns true if it is directory (excluding "." and ".." dirs");
+                 */
+                private final static FileFilter C_DIR_FILTER = new FileFilter() {
+                    @Override
+                    public boolean accept(File pathname) {
+                        String name = pathname.getName();
+                        if ( name.equals(".") || name.equals("..") ){
+                            return false;
+                        }
+                        return pathname.isDirectory();
+                    }
+                };
+                        
+
   
 }
